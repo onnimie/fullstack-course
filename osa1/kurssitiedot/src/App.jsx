7459c9b6daa6a33a1acba393c070ffb0dc937240
onnimiettinen <onnimie@gmail.com>
2024-07-02 18:26:57 +0300

    done 1.1

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -1,4 +1,4 @@
-const App = () => {
+const AppOLD = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
@@ -20,6 +20,45 @@ const App = () => {
         {part3} {exercises3}
       </p>
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <p>{props.part_name} {props.nof_exercises}</p>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.total}</p>
+  )
+}
+
+const App = () => {
+  // const-määrittelyt
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part_name={part1} nof_exercises={exercises1} />
+      <Content part_name={part2} nof_exercises={exercises2} />
+      <Content part_name={part3} nof_exercises={exercises3} />
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
