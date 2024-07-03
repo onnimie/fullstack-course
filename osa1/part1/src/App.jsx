925aea29b1f345544174c0aa278a3dc8b7cf8dbc
onnimiettinen <onnimie@gmail.com>
2024-07-03 23:05:08 +0300

    before 1.d

--- osa1/part1/src/App.jsx
+++ osa1/part1/src/App.jsx
@@ -36,13 +36,18 @@ export default App
 
 const Hello = (props) => {
 
-  console.log(props)
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - props.age
+  }
+
   return (
     <div>
       <p>
-
         Hello {props.name}, you are {props.age} years old
       </p>
+
+      <p>So you were probably born {bornYear()}</p>
     </div>
   )
 }
