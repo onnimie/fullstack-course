2db147923547cfc6e0c1a5b49d41c86b38ce143d
onnimiettinen <onnimie@gmail.com>
2024-07-15 16:33:17 +0300

    2.11 done (puhelinluettelo step6)

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
 const Entry = (props) => {
@@ -45,16 +46,19 @@ const EntryForm = (props) => {
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterword, setNewFilterword] = useState('')
+
+  useEffect(() => {
+    console.log("effect")
+    const p = axios.get('http://localhost:3001/persons')
+    p.then(res => {
+      console.log('promise fulfilled')
+      setPersons(res.data)
+    })
+  }, [])
 
   const handleNameInputChange = (event) => {
     console.log(event.target.value)
