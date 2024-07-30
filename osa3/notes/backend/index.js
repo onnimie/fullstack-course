fcf2deef2fe286e32451a36c98a87583e177820f
onnimiettinen <onnimie@gmail.com>
2024-07-30 20:50:57 +0300

    3c material until second wave of exercises

--- osa3/notes/backend/index.js
+++ osa3/notes/backend/index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
+
+const Note = require('./models/note')
 
 app.use(cors())
 app.use(express.json())
@@ -40,19 +43,17 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-    
-  
-    if (note) {
-      response.json(note)
-    } else {
-      response.status(404).end()
-    }
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  }).catch(err => {
+    response.status(404).end()
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -70,23 +71,20 @@ const generateId = () => {
 }
   
 app.post('/api/notes', (request, response) => {
-    const body = request.body
-  
-    if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
-    }
-  
-    const note = {
-      content: body.content,
-      important: body.important || false,
-      id: generateId(),
-    }
-  
-    notes = notes.concat(note)
-  
-    response.json(note)
+  const body = request.body
+
+  if (body.content) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 
@@ -99,7 +97,7 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
