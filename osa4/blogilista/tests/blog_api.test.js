15e12ab3d1042f7072ea58c623cc7bd2a8571075
onnimiettinen <onnimie@gmail.com>
2024-08-12 18:46:07 +0300

    testialustus

---
+++ osa4/blogilista/tests/blog_api.test.js
@@ -0,0 +1,14 @@
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+
+const helper = require('./test_helper')
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
