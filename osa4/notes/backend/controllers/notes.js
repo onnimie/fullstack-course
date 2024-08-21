5d541b2cae336b17585d3f983c27c8cac40854ef
onnimiettinen <onnimie@gmail.com>
2024-08-21 23:30:37 +0300

    4c material

--- osa4/notes/backend/controllers/notes.js
+++ osa4/notes/backend/controllers/notes.js
@@ -1,27 +1,41 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
+
 
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note.find({})
+  const notes = await Note
+    .find({}).populate('user', { username: 1, name: 1 })
+
   response.json(notes)
 })
+
 
 notesRouter.delete('/:id', async (request, response) => {
   await Note.findByIdAndDelete(request.params.id)
   response.status(204).end()
 })
 
+
 notesRouter.post('/', async (request, response) => {
   const body = request.body
+  const user = await User.findById(body.userId)
 
   const note = new Note({
     content: body.content,
-    important: body.important || false,
+    important: body.important === undefined ? false : body.important,
+
+    user: user._id
   })
 
   const savedNote = await note.save()
-  response.status(201).json(savedNote)
+
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
+  response.json(savedNote)
 })
+
 
 notesRouter.get('/:id', async (request, response) => {
   const note = await Note.findById(request.params.id)
@@ -31,6 +45,7 @@ notesRouter.get('/:id', async (request, response) => {
     response.status(404).end()
   }
 })
+
 
 notesRouter.put('/:id', (request, response, next) => {
   const body = request.body
