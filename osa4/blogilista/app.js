86f1d777a460320526be52ee7db2106fbdb888f2
onnimiettinen <onnimie@gmail.com>
2024-08-23 17:21:43 +0300

    4.15 done

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -3,7 +3,10 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 //const cors = require('cors')
+
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+
 //const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -23,5 +26,6 @@ mongoose.connect(config.MONGODB_URI)
 //app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
