eae4bcb506d34a45197d228b89b4fe844dbc896e
onnimiettinen <onnimie@gmail.com>
2024-08-23 18:06:06 +0300

    4.17 done

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,13 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
   response.json(blogs)
 })
   
 blogsRouter.post('/', async (request, response) => {
+
   let blog = new Blog(request.body)
   if (!blog.likes) {
     blog.likes = 0
@@ -15,14 +17,20 @@ blogsRouter.post('/', async (request, response) => {
 
   if (!blog.url || !blog.title) {
 
-    response.status(400).json({
+    return response.status(400).json({
       error: 'Missing fields (url or title)'
     })
-  } else {
-
-    const res = await blog.save()
-    response.status(201).json(res)
   }
+
+  const user = (await User.find({}))[0]
+  blog.user = user._id
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.status(201).json(savedBlog)
+  
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
