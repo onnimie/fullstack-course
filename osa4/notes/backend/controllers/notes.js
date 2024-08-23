2a3c0c530287e1024b6d151b40d248dc756e4391
onnimiettinen <onnimie@gmail.com>
2024-08-23 16:35:56 +0300

    4d material

--- osa4/notes/backend/controllers/notes.js
+++ osa4/notes/backend/controllers/notes.js
@@ -1,6 +1,16 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 
 notesRouter.get('/', async (request, response) => {
@@ -19,7 +29,13 @@ notesRouter.delete('/:id', async (request, response) => {
 
 notesRouter.post('/', async (request, response) => {
   const body = request.body
-  const user = await User.findById(body.userId)
+
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
 
   const note = new Note({
     content: body.content,
