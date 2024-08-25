ed941e2a97a1886bb9677fc139a462f76388b84e
onnimiettinen <onnimie@gmail.com>
2024-08-25 12:35:04 +0300

    4.18 done

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -6,6 +6,7 @@ const app = express()
 
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 //const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
@@ -27,5 +28,6 @@ mongoose.connect(config.MONGODB_URI)
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
